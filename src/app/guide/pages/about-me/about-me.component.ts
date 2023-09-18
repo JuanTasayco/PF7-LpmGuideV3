79a7d7c21dc65e7221dd8f8b5542a69857eb9f7e
Juan Tasayco <juantasayco266@gmail.com>
2023-09-18 18:40:15 -0500

    addAboutMe

--- src/app/guide/pages/about-me/about-me.component.ts
+++ src/app/guide/pages/about-me/about-me.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SectionsComponent } from '../../components/sections/sections.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [CommonModule],
+  imports: [SectionsComponent],
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
