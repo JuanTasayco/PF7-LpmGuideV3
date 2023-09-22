6ad33d1b9f00a507323ab4b82640e2a41ba06f1a
Juan Tasayco <juantasayco266@gmail.com>
2023-09-22 18:25:04 -0500

    addContentConcept

--- src/app/guide/pages-parts/part-concept/part-concept.component.ts
+++ src/app/guide/pages-parts/part-concept/part-concept.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { CommonModule, JsonPipe } from '@angular/common';
+import { CommonModule, JsonPipe, NgFor } from '@angular/common';
 import { InfoSectionsService } from '../../services/info-sections.service';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs';
@@ -8,12 +8,19 @@ import { Seccion } from '../../interfaces/sections.interfaces';
 @Component({
   selector: 'app-part-concept',
   standalone: true,
-  imports: [JsonPipe],
+  imports: [JsonPipe,NgFor],
   templateUrl: './part-concept.component.html',
   styleUrls: ['./part-concept.component.scss'],
 })
 export class PartConceptComponent implements OnInit {
-  public concepto = signal<Seccion | ''>('');
+  public concepto = signal<Seccion>({
+    titulo: '',
+    titulo2: '',
+    subtitulo: '',
+    panel: '',
+    seccion: '',
+
+  });
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
