63878544a05360b2be69e6ceef3d7bbd99b67947
Juan Tasayco <juantasayco266@gmail.com>
2023-09-25 18:17:37 -0500

    fixModalZoom

--- src/app/guide/components/modal/modal.component.ts
+++ src/app/guide/components/modal/modal.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Flowbite } from '../../decorator/flowbite-decorator';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-modal',
@@ -14,4 +15,35 @@ export class ModalComponent {
   @Input('modelTargetComponent') modelTarget: string = '';
   @Input('linkImageComponent') linkImage: string = '';
   @Input('numberImageComponent') numberImage: number = 0;
+
+  @ViewChild('imageToScale') image!: ElementRef<HTMLElement>;
+  private scaleNumber: number = 1;
+  retireScale() {
+    console.log('hello');
+    this.scaleNumber = 1;
+    gsap.set(this.image.nativeElement, {
+      scaleX: 1,
+      scaleY: 1,
+    });
+  }
+  clickEventScaleReduce() {
+    if (this.scaleNumber > 1) {
+      this.scaleNumber -= 0.1;
+    }
+    this.updateScale();
+  }
+
+  clickEventScaleIncrease() {
+    if (this.scaleNumber < 1.5) {
+      this.scaleNumber += 0.1;
+    }
+    this.updateScale();
+  }
+
+  updateScale() {
+    gsap.to(this.image.nativeElement, {
+      scaleY: this.scaleNumber,
+      scaleX: this.scaleNumber,
+    });
+  }
 }
