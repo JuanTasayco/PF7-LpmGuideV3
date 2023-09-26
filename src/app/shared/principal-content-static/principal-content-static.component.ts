921d8e863721c5c340d3a1c0ad75259bb4ea5229
Juan Tasayco <juantasayco266@gmail.com>
2023-09-26 12:30:49 -0500

    fixMockup

--- src/app/shared/principal-content-static/principal-content-static.component.ts
+++ src/app/shared/principal-content-static/principal-content-static.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MockupMobileComponent } from '../mockup-mobile/mockup-mobile.component';
 
 @Component({
   selector: 'app-principal-content-static',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MockupMobileComponent],
   templateUrl: './principal-content-static.component.html',
   styleUrls: ['./principal-content-static.component.scss']
 })
