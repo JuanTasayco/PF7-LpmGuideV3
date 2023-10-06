3ec3b4768885d2ca10e7bce9d47d6d9e423e5d9a
Juan Tasayco <juantasayco266@gmail.com>
2023-10-06 18:02:43 -0500

    addLogicFormSectionAdd

--- src/app/admin/pages/sections/sections-user.component.ts
+++ src/app/admin/pages/sections/sections-user.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { NgFor, TitleCasePipe } from '@angular/common';
+import { Component, OnInit, computed, signal } from '@angular/core';
+import { JsonPipe, NgFor, NgIf, TitleCasePipe } from '@angular/common';
 import { InfoSectionsService } from 'src/app/guide/services/info-sections.service';
 import { Seccion } from 'src/app/guide/interfaces/sections.interfaces';
+import { ActivatedRoute } from '@angular/router';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+export interface Personaje {
+  name: string;
+  lastName: string;
+}
 
 @Component({
   selector: 'app-sections',
   standalone: true,
-  imports: [NgFor, TitleCasePipe],
+  imports: [NgFor, NgIf, TitleCasePipe, ReactiveFormsModule, JsonPipe],
   templateUrl: './sections-user.component.html',
   styleUrls: ['./sections-user.component.scss'],
 })
@@ -22,11 +37,65 @@ export class SectionsUserComponent implements OnInit {
     contenido: [],
   });
 
+  public sectionsForm: FormGroup = this.formBuilder.group({
+    id: [''],
+    titulo: [''],
+    titulo2: [''],
+    subtitulo: [''],
+    panel: [''],
+    seccion: [''],
+    ingreso: this.formBuilder.array([], Validators.required),
+    contenido: this.formBuilder.array([], Validators.required),
+  });
+
+  formForArray: FormGroup = this.formBuilder.group({
+    subtitles: ['', Validators.required],
+    imagesUrl: [''],
+  });
+
+  get ingresoArray() {
+    return this.sectionsForm.get('ingreso') as FormArray;
+  }
+
+  get contenidoArray() {
+    return this.sectionsForm.get('contenido') as FormArray;
+  }
+
+  public addFormGroupToArray(currentArray: FormArray) {
+    currentArray.push(
+      this.formBuilder.group({
+        subtitles: [this.formForArray.get('subtitles')?.value ?? ''],
+        imagesUrl: [this.formForArray.get('imagesUrl')?.value ?? ''],
+      })
+    );
+    this.formForArray.reset();
+  }
+
+  public deleteFormGroupToArray(pos: number, currentArray: FormArray) {
+    currentArray.removeAt(pos);
+  }
+
+  sendForm() {
+    console.log(this.sectionsForm.value);
+  }
+
   get getKeysSeccion() {
     return Object.keys(this.seccion());
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.activatedRouter.params.subscribe(({ id }) => {
+      if (id) {
+        console.log('estoy editando');
+      } else {
+        console.log('estoy agregando');
+      }
+    });
+  }
 
-  constructor(private guideService: InfoSectionsService) {}
+  constructor(
+    private guideService: InfoSectionsService,
+    private activatedRouter: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
 }
--- src/app/shared/nav-bar-admin/nav-bar-admin.component.ts
+++ src/app/shared/nav-bar-admin/nav-bar-admin.component.ts
@@ -4,9 +4,9 @@ import {
   ViewChild,
   AfterViewInit,
   Renderer2,
+  ViewEncapsulation,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { gsap } from 'gsap';
 import { Flowbite } from 'src/app/guide/decorator/flowbite-decorator';
 @Component({
   selector: 'app-nav-bar-admin',
@@ -14,6 +14,7 @@ import { Flowbite } from 'src/app/guide/decorator/flowbite-decorator';
   imports: [CommonModule],
   templateUrl: './nav-bar-admin.component.html',
   styleUrls: ['./nav-bar-admin.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
 @Flowbite()
 export class NavBarAdminComponent implements AfterViewInit {
