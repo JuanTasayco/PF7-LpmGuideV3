6fbffdf57d17f3c35bfacbbe4a46a86ad282bd7c
JuanTasayco <juantasayco266@gmail.com>
2023-10-09 08:47:15 -0500

    addEditUserSection

--- src/app/admin/pages/users/users.component.ts
+++ src/app/admin/pages/users/users.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { EditUserComponent } from '../../components/edit-user/edit-user.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [EditUserComponent],
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
