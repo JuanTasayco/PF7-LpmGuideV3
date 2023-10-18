8358aa92c7016c02baf05b34903183e7e9eee00d
JuanTasayco <juantasayco266@gmail.com>
2023-10-17 23:14:09 -0500

    emitEvent

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,6 +1,14 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { gsap } from 'gsap';
+import { Observable, Subject, debounceTime } from 'rxjs';
 @Component({
   selector: 'app-search',
   standalone: true,
@@ -8,6 +16,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements AfterViewInit, OnInit {
+  activatedSearch: boolean = false;
+
   @Input('place') placeholderName: string = '';
+  @Output() eventValueText = new EventEmitter<string>();
+
+  obsTextUser = new Subject<string>();
+
+  getSearchUser(eventUser: Event) {
+    if (eventUser) {
+    }
+    const { value: valueText } = eventUser?.target as HTMLInputElement;
+    this.obsTextUser.next(valueText);
+  }
+
+  ngOnInit(): void {
+    this.obsTextUser.pipe(debounceTime(500)).subscribe((text) => {
+      console.log(text);
+      this.eventValueText.emit(text);
+    });
+  }
+
+  ngAfterViewInit(): void {}
 }
